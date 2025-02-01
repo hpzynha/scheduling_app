1ec3bbbd3635f796e47bafbd156f0720fa2f2bbd
hpzynha <larissa.nogueira.rocha@gmail.com>
2025-02-01 13:36:05 -0400

    model

---
+++ lib/models/scheduling.dart
@@ -0,0 +1,15 @@
+class Scheduling {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime date;
+  final String contact;
+
+  Scheduling({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.contact,
+  });
+}
